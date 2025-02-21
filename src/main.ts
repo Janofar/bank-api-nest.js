774f71348a61c9f01b2ca42632965ac5dda94ca7
JanofarJahan <janofarjahan2000@gmail.com>
2025-02-21 21:15:40 +0530

    swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
     origin :'*',
     credentials: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('Bank API')
+    .setDescription('API Documentation for Bank App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
